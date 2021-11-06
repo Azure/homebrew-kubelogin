1cbbf273cdccbc1bf6e065969d6b2ba524168ddb
Damien Nozay <dnozay@users.noreply.github.com>
2021-11-05 18:49:42 -0700

    add M1 support to original recipe too

--- Formula/kubelogin.rb
+++ Formula/kubelogin.rb
@@ -7,21 +7,30 @@ class Kubelogin < Formula
   version "0.0.10"
   license "MIT"
 
-  if OS.mac?
+  case
+  when OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-amd64.zip"
     sha256 "e4d155139d36fc489d4366ca818c9c400bf35ebe347b0f1d5bb84864b406d420"
-  end
-  if OS.linux?
+  when OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-arm64.zip"
+    sha256 "793f72b1d757a1404169680ffc35427c2d62e557a88503dc74f43bb9bfedf2b5"
+  when OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-linux-amd64.zip"
     sha256 "2098de5b617fcfc1653c435488a2fc4cd32bdd2450fee17cdf5b1b3c08f6889b"
+  else
+    odie "Unexpected platform!"
   end
 
   def install
-    if OS.mac?
+    case
+    when OS.mac? && Hardware::CPU.intel?
       bin.install "darwin_amd64/kubelogin"
-    end
-    if OS.linux?
+    when OS.mac? && Hardware::CPU.arm?
+      bin.install "darwin_arm64/kubelogin"
+    when OS.linux? && Hardware::CPU.intel?
       bin.install "linux_amd64/kubelogin"
+    else
+      odie "Unexpected platform!"
     end
   end
 
