01d26d53944a33e117d00c5e124acdcb91d8d559
Weinong Wang <weinongw@microsoft.com>
2022-12-21 13:53:55 +0800

    v0.0.25 release (#11)

--- Formula/az-kubelogin.rb
+++ Formula/az-kubelogin.rb
@@ -10,13 +10,13 @@ class AzKubelogin < Formula
   case
   when OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-amd64.zip"
-    sha256 "57af8b77b79dc31d50d8ee5b716f03ddedd4156154e3ce61ce9441d0954f7f18"
+    sha256 "d9670a5817978a6d27ac6f833df40b407b2c2dee170dec167996bcaf7062af31"
   when OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-arm64.zip"
-    sha256 "9d260446b5d61e4b5e864b5894808adc40f33d4f4340a371af20397cc9038158"
+    sha256 "c03bc406efd04418d5695667d27040f99dae578e4202f4c3d4bf7d6bf1560264"
   when OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-linux-amd64.zip"
-    sha256 "1350c5e2f02f3a4fa2c13c3ae15f83383b0baf74725a2de2460b5e78f2c68e36"
+    sha256 "4cb85f62f53209fd845514e8f9729ad4c818ac662e3c717958fc22f1a971850e"
   else
     odie "Unexpected platform!"
   end
