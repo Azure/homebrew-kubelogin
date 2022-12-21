9b6bae29bb77ab6727cd4a6f737bf7e66627c3b4
Weinong Wang <weinongw@microsoft.com>
2022-12-21 14:03:37 +0800

    fix the missing version

--- Formula/kubelogin.rb
+++ Formula/kubelogin.rb
@@ -4,7 +4,7 @@
 class Kubelogin < Formula
   desc "A Kubernetes credential (exec) plugin implementing azure authentication"
   homepage "https://github.com/Azure/kubelogin"
-  version "0.0.20"
+  version "0.0.25"
   license "MIT"
 
   case
