bad4606fea929ae516da770efa07637ba89613f0
Weinong Wang <weinongw@microsoft.com>
2023-12-28 12:16:26 -0800

    v0.1.0 release

--- Formula/az-kubelogin.rb
+++ Formula/az-kubelogin.rb
@@ -4,19 +4,19 @@
 class AzKubelogin < Formula
   desc "A Kubernetes credential (exec) plugin implementing azure authentication"
   homepage "https://github.com/Azure/kubelogin"
-  version "0.0.34"
+  version "0.1.0"
   license "MIT"
 
   case
   when OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-amd64.zip"
-    sha256 "792511a64aab2adf531a57ce86232fda117a70c0ca28c05f4f1fda6a14da2ddb"
+    sha256 "74eafe2cb61fc678aa0053b83964c56e18e1b72b117bbe66e3014b5dfbb4b0ec"
   when OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-arm64.zip"
-    sha256 "d2536eb23ff96e0bf59e86c2ec083264ab4fd9345dc3c132f47617484a8842f0"
+    sha256 "f03a1ac8ac2a94d617ef6753b980ba19f63a2f80324c3cf7dc5892b602ef0bf3"
   when OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-linux-amd64.zip"
-    sha256 "de2a529b4b044f6d41b82cd256c9d9fba44a68ad3c48f2d68e1c093eba51db02"
+    sha256 "be6247bed2a901b986c7edf2b106618480eb60aa3a47708149f0c62e58ebbab9"
   else
     odie "Unexpected platform!"
   end
