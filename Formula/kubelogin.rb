8017aef4c341003e58666fe920d60aa15318db97
bcho <1975118+bcho@users.noreply.github.com>
2026-05-30 09:42:56 +0000

    Bump kubelogin to v0.2.18

--- Formula/kubelogin.rb
+++ Formula/kubelogin.rb
@@ -4,19 +4,19 @@
 class Kubelogin < Formula
   desc "A Kubernetes credential (exec) plugin implementing azure authentication"
   homepage "https://github.com/Azure/kubelogin"
-  version "0.2.17"
+  version "0.2.18"
   license "MIT"
 
   case
   when OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-amd64.zip"
-    sha256 "db7f3498667e9cabbba72f139a49cb6a43ba07904c29edb0c246a987c90b1183"
+    sha256 "602e7d85b49ebbae740d89c7c0932d58f972c7607d5725f095810926b988b000"
   when OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-darwin-arm64.zip"
-    sha256 "c10f9f34bfdb32894ed05a6be39262bca0d4a2fd47f59d8fa1aa7b5216f92f91"
+    sha256 "025550905a01921d1262a9a3cce1ae9fe054668830de8ab37c7fdcfc08b09840"
   when OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Azure/kubelogin/releases/download/v#{version}/kubelogin-linux-amd64.zip"
-    sha256 "9a5484504ab1c91cdafdcd55a9e439bb3f55a0fe29fe253a169df5538c0a2c86"
+    sha256 "33e2907cc9aeed8f84a467bdccf0c87707a0961e4eac19224846cc1f5a4d7807"
   else
     odie "Unexpected platform!"
   end
